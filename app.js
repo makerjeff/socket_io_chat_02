275a29e5e848b0fbbd5244d3c690e5b6fa1e4c30
Jeff Wu <makerjeffwu@gmail.com>
2015-12-01 22:17:33 -0800

    everything is working, including wonderful colorful console dumps

--- app.js
+++ app.js
@@ -11,6 +11,25 @@ var port = 3000;    //what port?
 server.listen(port);    //tell server to listen in on <port>
 console.log(colors.rainbow('server listening on port ') + port.toString().green);
 
+//SERVE HTML
 app.get('/', function(request, response){
     response.sendFile(__dirname + '/index.html');
+});
+
+//OPEN SOCKET, io.sockets.on = wrapper function
+io.sockets.on('connection', function(socket) {
+
+    console.log('Client logged in with ID: ' + socket.id.toString().green + '. ');
+
+    //custom event
+    socket.on('send message', function(data) {
+        //io.sockets.emit (<string:event>, <data to pass>);
+        io.sockets.emit('new message',  "<b>" +socket.id + "</b>" + " says: " + data);   //this needs to be received by the client side
+        console.log(socket.id.toString().green + ' says: ' + data);
+    });
+
+    //disconnect event
+    socket.on('disconnect', function(data){
+        console.log('client' + socket.id.toString().red + ' disconnected');
+    });
 });
